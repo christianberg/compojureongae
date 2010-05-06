884abf37401c6cc84ec475d84c79762c3a994b5d
Christian Berg <berg.christian@gmail.com>
2010-05-06 20:26:00 +0200

    Started over using Leiningen by calling 'line new'.

---
+++ src/compojureongae/core.clj
@@ -0,0 +1,1 @@
+(ns compojureongae.core)
---
+++ test/compojureongae/core_test.clj
@@ -0,0 +1,6 @@
+(ns compojureongae.core-test
+  (:use [compojureongae.core] :reload-all)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false))
