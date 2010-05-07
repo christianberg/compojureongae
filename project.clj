24e398e7c6dd3787c8f0ac77585939cf665c0d95
Christian Berg <berg.christian@gmail.com>
2010-05-07 14:17:37 +0200

    Compojure hello world app. [Finishes #3426524]

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject compojureongae "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]])
+(defproject compojureongae "0.1.0-SNAPSHOT"
+  :description "Example app for deployoing Compojure on Google App Engine"
+  :dependencies [[compojure "0.4.0-SNAPSHOT"]
+                 [ring/ring-jetty-adapter "0.2.0"]]
+  :dev-dependencies [[leiningen/lein-swank "1.1.0"]])
--- src/compojureongae/core.clj
+++ src/compojureongae/core.clj
@@ -1,1 +1,10 @@
-(ns compojureongae.core)
+(ns compojureongae.core
+  (:use compojure.core
+        ring.adapter.jetty)
+  (:require [compojure.route :as route]))
+
+(defroutes example
+  (GET "/" [] "<h1>Hello World Wide Web!</h1>")
+  (route/not-found "Page not found"))
+
+(run-jetty example {:port 8080})
