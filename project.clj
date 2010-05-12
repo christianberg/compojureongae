28dc59ff753e41eb4dcd55f4f0e259be63aeceac
Christian Berg <berg.christian@gmail.com>
2010-05-12 12:53:43 +0200

    Simplified build process, removing the dev-dependency on leiningen-war.

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :namespaces [compojureongae.core]
   :dependencies [[compojure "0.4.0-SNAPSHOT"]
                  [ring/ring-servlet "0.2.1"]]
-  :dev-dependencies [[leiningen/lein-swank "1.2.0-SNAPSHOT"]
-                     [uk.org.alienscience/leiningen-war "0.0.2"]])
+  :dev-dependencies [[leiningen/lein-swank "1.2.0-SNAPSHOT"]]
+  :compile-path "war/WEB-INF/classes"
+  :library-path "war/WEB-INF/lib")
