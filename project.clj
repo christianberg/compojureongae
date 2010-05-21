0b63bfc18c80604d97bcb581b94941ecf3b196b2
Christian Berg <berg.christian@gmail.com>
2010-05-21 14:13:32 +0200

    lein-swank was moved to swank-clojure

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :namespaces [compojureongae.core]
   :dependencies [[compojure "0.4.0-SNAPSHOT"]
                  [ring/ring-servlet "0.2.1"]]
-  :dev-dependencies [[leiningen/lein-swank "1.2.0-SNAPSHOT"]]
+  :dev-dependencies [[swank-clojure "1.2.0"]]
   :compile-path "war/WEB-INF/classes"
   :library-path "war/WEB-INF/lib")
