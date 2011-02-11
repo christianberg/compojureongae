dea0337a711eee5d723c9ea34d70179c1f3e0300
Christian Berg <berg.christian@gmail.com>
2011-02-11 14:18:58 +0100

    Updated project.clj to work with current Leiningen version.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject compojureongae "0.3.1"
+(defproject compojureongae "0.3.2"
   :description "Example app for deployoing Compojure on Google App Engine"
-  :namespaces [compojureongae.core]
+  :aot [compojureongae.core]
   :dependencies [[compojure "0.4.0-RC3"]
                  [ring/ring-servlet "0.2.1"]
                  [hiccup "0.2.4"]
@@ -11,6 +11,6 @@
                      [ring/ring-jetty-adapter "0.2.0"]
                      [com.google.appengine/appengine-local-runtime "1.3.4"]
                      [com.google.appengine/appengine-api-stubs "1.3.4"]]
-  :repositories [["maven-gae-plugin" "http://maven-gae-plugin.googlecode.com/svn/repository"]]
+  :repositories {"maven-gae-plugin" "http://maven-gae-plugin.googlecode.com/svn/repository"}
   :compile-path "war/WEB-INF/classes"
   :library-path "war/WEB-INF/lib")
